3922ea08683bfbd3d17fb27ec04b1bacda641f53
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2024-07-05 03:38:35 +0900

    add: yew

--- dockerfile
+++ dockerfile
@@ -3,8 +3,9 @@ FROM rust:latest
 RUN apt update && \
     apt install vim clang cmake libssl-dev build-essential postgresql-client -y
 RUN rustup component add rls rust-analysis rust-src rustfmt clippy && \
-    cargo install cargo-edit cargo-watch cargo-make sqlx-cli wasm-pack && \
-    rustup install nightly
+    cargo install cargo-edit cargo-watch cargo-make sqlx-cli wasm-pack trunk && \
+    rustup install nightly && \
+    rustup target add wasm32-unknown-unknown
 WORKDIR /workspace
 ENV USER=root
 ENV RUST_BACKTRACE=1
--- rust-todo-frontend/src/main.rs
+++ rust-todo-frontend/src/main.rs
@@ -1,3 +1,22 @@
+use gloo::console;
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    console::log!("Hello log!");
+    console::debug!("Hello debug!");
+    console::info!("Hello info!");
+    console::warn!("Hello warn!");
+    console::assert!(true, "Hello world!");
+    console::assert!(false, "This will fail!");
+    console::table!([0, 1, 2], ["a", "b"]);
+    html! {
+        <div>
+            {"hello yew"}
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
